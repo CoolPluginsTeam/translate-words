ba5027fc6de623e32bbe105541e0f6e7f4fd9786
VishaliCoolPlugins <vishalicoolplugins@gmail.com>
2025-12-30 11:51:03 +0530

    update build

--- admin/assets/bulk-translate/index.asset.php
+++ admin/assets/bulk-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '65668494dccdae81f677');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '03964df1dc4332ab4bd5');
--- admin/assets/page-translation/index.asset.php
+++ admin/assets/page-translation/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '71cd037967fffb82fdcd');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '541e138733453fc75679');
