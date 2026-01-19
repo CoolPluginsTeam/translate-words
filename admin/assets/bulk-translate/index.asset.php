fdc8f611d9d61c54bc3ca6a7d232e3b7c9f81163
sushil <sushilcoolplugins@gmail.com>
2026-01-19 12:14:33 +0530

    Added build files

--- admin/assets/bulk-translate/index.asset.php
+++ admin/assets/bulk-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '03964df1dc4332ab4bd5');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'd2d7a0fdd48f79a8aebb');
