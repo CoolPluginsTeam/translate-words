d7b379c39a57cb6bd461fd910ab3c01c323ed6bc
VishaliCoolPlugins <vishalicoolplugins@gmail.com>
2026-07-30 16:51:32 +0530

    update build

--- admin/assets/bulk-translate/index.asset.php
+++ admin/assets/bulk-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '96b84f43411cc66c1545');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'b34b46d8898a4d33177c');
